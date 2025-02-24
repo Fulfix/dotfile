9e275c1a0d97fa93cf39fd0cf440c5f1c4fa5cf9
fulfix <fulfix.sellier@gmail.com>
2025-02-24 13:44:28 +0100

    fix theme switcher user error

--- theme_switcher/change_theme.sh
+++ theme_switcher/change_theme.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 set -e
-
-ls /home/thomas/theme/my_themes
+if [ "$(whoami)" = 'root' ]; then
+    exit 1
+fi
+user=$(echo $USER)
+echo $user
+ls /home/$user/theme/my_themes
 read -e -p "choose your theme: " new_theme
 
 # Input colors
-old_font_color=$(jq -r ".font" /home/thomas/theme/current_colors.json)
-old_accent_color=$(jq -r ".accent" /home/thomas/theme/current_colors.json)
-old_bg_color=$(jq -r ".bg" /home/thomas/theme/current_colors.json)
-old_term_accent_color=$(jq -r ".term_accent" /home/thomas/theme/current_colors.json)
-old_term_font_color=$(jq -r ".term_font" /home/thomas/theme/current_colors.json)
-old_wallpaper=$(jq -r ".wallpaper" /home/thomas/theme/current_colors.json)
+old_font_color=$(jq -r ".font" /home/$user/theme/current_colors.json)
+old_accent_color=$(jq -r ".accent" /home/$user/theme/current_colors.json)
+old_bg_color=$(jq -r ".bg" /home/$user/theme/current_colors.json)
+old_term_accent_color=$(jq -r ".term_accent" /home/$user/theme/current_colors.json)
+old_term_font_color=$(jq -r ".term_font" /home/$user/theme/current_colors.json)
+old_wallpaper=$(jq -r ".wallpaper" /home/$user/theme/current_colors.json)
 
-new_font_color=$(jq -r ".font" /home/thomas/theme/my_themes/"$new_theme")
-new_accent_color=$(jq -r ".accent" /home/thomas/theme/my_themes/"$new_theme")
-new_bg_color=$(jq -r ".bg" /home/thomas/theme/my_themes/"$new_theme")
-new_term_accent_color=$(jq -r ".term_accent" /home/thomas/theme/my_themes/"$new_theme")
-new_term_font_color=$(jq -r ".term_font" /home/thomas/theme/my_themes/"$new_theme")
-new_wallpaper=$(jq -r ".wallpaper" /home/thomas/theme/my_themes/"$new_theme")
+new_font_color=$(jq -r ".font" /home/$user/theme/my_themes/"$new_theme")
+new_accent_color=$(jq -r ".accent" /home/$user/theme/my_themes/"$new_theme")
+new_bg_color=$(jq -r ".bg" /home/$user/theme/my_themes/"$new_theme")
+new_term_accent_color=$(jq -r ".term_accent" /home/$user/theme/my_themes/"$new_theme")
+new_term_font_color=$(jq -r ".term_font" /home/$user/theme/my_themes/"$new_theme")
+new_wallpaper=$(jq -r ".wallpaper" /home/$user/theme/my_themes/"$new_theme")
 
 # Use double quotes to prevent issues with variables
 echo "{
@@ -27,24 +31,24 @@ echo "{
   \"term_accent\": \"$new_term_accent_color\",
   \"term_font\": \"$new_term_font_color\",
   \"wallpaper\": \"$new_wallpaper\"
-}" > /home/thomas/theme/current_colors.json
+}" > /home/$user/theme/current_colors.json
 
 # Backup configuration
-backup_dir="/home/thomas/theme/config_backup"
+backup_dir="/home/$user/theme/config_backup"
 mkdir -p "$backup_dir"
 for dir in ".config" ".vim" ".mozilla"; do
-    sudo cp -r "/home/thomas/$dir" "$backup_dir"
+    sudo cp -r "/home/$user/$dir" "$backup_dir"
 done
 sudo cp -r /usr/share/sddm/themes/simple-sddm-2/ "$backup_dir"
 
 # Change wallpaper
 swww img --resize crop -t random $new_wallpaper
-magick $new_wallpaper  -blur 0x30 /home/thomas/.config/wlogout/icons/blur.jpg
+magick $new_wallpaper  -blur 0x30 /home/$user/.config/wlogout/icons/blur.jpg
 # Replace colors in specified directories
 target_dirs=(
-    "/home/thomas/.config"
-    "/home/thomas/.mozilla"
-    "/home/thomas/.vim/colors"
+    "/home/$user/.config"
+    "/home/$user/.mozilla"
+    "/home/$user/.vim/colors"
     "/usr/share/sddm/themes/simple-sddm-2/theme.conf"
 )
 
@@ -55,14 +59,14 @@ for dir in "${target_dirs[@]}"; do
     -e "s/$old_accent_color/$new_accent_color/g" {} +
 done
 
-echo "DIR 38;2;$(dye -x rgb "$new_accent_color" | tr -d "rgb()," | sed "s/ /;/g")" | sudo tee /home/thomas/.config/dircolor > /dev/null
+echo "DIR 38;2;$(dye -x rgb "$new_accent_color" | tr -d "rgb()," | sed "s/ /;/g")" | sudo tee /home/$user/.config/dircolor > /dev/null
 
-sudo sed -i "s/$old_term_accent_color/$new_term_accent_color/g" /home/thomas/.config/ranger/colorschemes/custom.py
-sudo sed -i "s/$old_term_font_color/$new_term_font_color/g" /home/thomas/.config/ranger/colorschemes/custom.py
+sudo sed -i "s/$old_term_accent_color/$new_term_accent_color/g" /home/$user/.config/ranger/colorschemes/custom.py
+sudo sed -i "s/$old_term_font_color/$new_term_font_color/g" /home/$user/.config/ranger/colorschemes/custom.py
 
 # Fix permissions
 for dir in ".config" "theme/config_backup/.mozilla" "theme/config_backup/.config" "theme/config_backup/.vim"; do
-    sudo chown -R thomas:thomas "/home/thomas/$dir"
+    sudo chown -R $user:$user "/home/$user/$dir"
 done
 
 # Restart Waybar & SwayNC
